49777eb320ef86d4ca5d8a6b6eaba5389c7fdaff
mayorD19 <dominionoje@gmail.com>
2023-09-11 21:59:28 +0100

    HNG_TASK!

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import *
+import json
+from datetime import datetime
+
+
+# initialization of an instance of the flask object
+app = Flask(__name__)
+
+
+@app.route("/api", methods=["GET"])
+def home_page():
+    # Getting the query parameters from the request
+    slack_name = request.args.get("slack_name")
+    track = request.args.get("track")
+
+    data_set = {"slack_name": slack_name,
+                "current_day": datetime.utcnow().strftime('%A'),
+                "utc_time": datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S UTC'),
+                "Track": track,
+                "github_file_url": "",
+                "github_repo_url": "",
+                "status_code": 200}
+    json_dump = json.dumps(data_set)
+
+    return json_dump
+
+
+if __name__ == "__main__":
+    app.run()
+
